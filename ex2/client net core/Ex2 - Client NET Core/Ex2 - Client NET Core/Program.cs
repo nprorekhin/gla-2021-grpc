ab615b25ae36299ac2b9c11f3466919694fab990
Nikita Prorekhin <nikita.prorekhin@noffz.com>
2021-11-13 21:50:43 +0100

    Adds dotnet 5.0 example client for Ex 2, updates gitignore
    Signed-off-by: Nikita Prorekhin <nikita.prorekhin@noffz.com>

---
+++ ex2/client net core/Ex2 - Client NET Core/Ex2 - Client NET Core/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Grpc.Net.Client; 
+
+
+namespace Ex2___Client_NET_Core
+{
+    class Program
+    {
+
+        static string defaultAddress = "http://localhost:50051";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Welcome to Temperature Controller ****\n\n");
+
+            Console.Write($"Please enter the address of the server or press Enter to use default '{defaultAddress}': ");
+            var address = Console.ReadLine();
+            if (String.IsNullOrEmpty(address))
+                address = defaultAddress;
+
+            var channel = GrpcChannel.ForAddress(address);
+            var client = new TemperatureControllerService.TemperatureControllerServiceClient(channel);
+            Console.WriteLine($"\nReady to control '{address}'.");
+
+            var temperature = client.Measure(new MeasureRequest() { Unit = TemperatureUnit.Celsius }).Temperature;
+            Console.WriteLine($"\tMeasured temperature is : {temperature}");
+
+            var controllerSettings = new ControllerSettingsRequest() 
+            {
+                TemperatureSetpoint = 30.12, 
+                Unit = TemperatureUnit.Celsius, 
+                Enabled = true 
+            };
+            Console.WriteLine($"\tSetting temperature to: {controllerSettings.TemperatureSetpoint} {controllerSettings.Unit}.");
+            client.Configure(controllerSettings);
+
+            temperature = client.Measure(new MeasureRequest() { Unit = TemperatureUnit.Celsius }).Temperature;
+            Console.WriteLine($"\tMeasured temperature is : {temperature}");
+
+            controllerSettings.Enabled = false;
+            client.Configure(controllerSettings);
+            Console.WriteLine($"\nThank you for using our client to control '{address}'! Press any key to exit. ");
+            Console.ReadKey();
+        }
+    }
+}
